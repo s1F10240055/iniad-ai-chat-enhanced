ac930f96895f45174e6a8e9d05f7bdc6b16595db
s1F10240055 <s1F102400550@iniad.org>
2026-04-27 15:09:18 +0900

    fix: add missing config object for maker-zip in forge configuration

--- forge.config.ts
+++ forge.config.ts
@@ -20,6 +20,7 @@ const config: ForgeConfig = {
     {
       name: "@electron-forge/maker-zip",
       platforms: ["darwin"],
+      config: {},
     },
     {
       name: "@electron-forge/maker-deb",
